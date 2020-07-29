edbf448cf8743d38d71bd767aab5797a6cd91ccf
Alex Curnow <alexcurnow@gmail.com>
2020-07-29 14:02:39 -0500

    got image to show up on canvas

--- Sender/client/src/components/climb/BetaBuilder.js
+++ Sender/client/src/components/climb/BetaBuilder.js
@@ -1,23 +1,47 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { ClimbContext } from "../../providers/ClimbProvider";
 import { useParams } from "react-router-dom";
+import "./BetaBuilder.css";
 
 export const BetaBuilder = () => {
   const { id } = useParams();
   const parsedId = parseInt(id);
-  const { currentClimb, climbs, getAllClimbs, getClimbById } = useContext(
-    ClimbContext
-  );
+  const { currentClimb, getClimbById } = useContext(ClimbContext);
+
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const imageRef = useRef(null);
 
   useEffect(() => {
-    getAllClimbs();
     getClimbById(parsedId);
+
+    const canvas = canvasRef.current;
+    canvas.style.height = "1000px";
+    canvas.style.width = "800px";
+    // canvas.style.border = "1px solid black";
+
+    const ctx = canvas.getContext("2d");
+    contextRef.current = ctx;
+    // ctx.imageSmoothingEnabled = false;
+
+    const img = imageRef.current;
+
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
   }, []);
 
-  console.log(climbs);
-  console.log(currentClimb);
-
-  const jsx = <img src={currentClimb.imageUrl} />;
-
-  return <>{jsx}</>;
+  return (
+    <>
+      <canvas ref={canvasRef}></canvas>
+      <img
+        className="hidden"
+        ref={imageRef}
+        src={currentClimb.imageUrl}
+        alt="climbing problem"
+      />
+    </>
+  );
 };
