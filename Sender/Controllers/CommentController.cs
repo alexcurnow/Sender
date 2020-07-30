4702da16359af2bfeb50acf71ddba9974f29af61
Alex Curnow <alexcurnow@gmail.com>
2020-07-30 15:12:37 -0500

    building out comments

---
+++ Sender/Controllers/CommentController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sender.Data;
+using Sender.Models;
+using Sender.Repositories;
+
+namespace Sender.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly CommentRepository _commentRepository;
+        public CommentController(ApplicationDbContext context)
+        {
+            _commentRepository = new CommentRepository(context);
+        }
+
+        [HttpGet("getByClimb/{id}")]
+        public IActionResult GetByClimbId(int id)
+        {
+            return Ok(_commentRepository.GetCommentsByClimbId(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Comment comment)
+        {
+            comment.DateCreated = DateTime.Now;
+            _commentRepository.AddComment(comment);
+            return CreatedAtAction("Get", new { id = comment.Id }, comment);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _commentRepository.DeleteComment(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+            _commentRepository.Update(comment);
+            return NoContent();
+
+        }
+    }
+}
---
+++ Sender/Repositories/CommentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sender.Data;
+using Sender.Models;
+
+namespace Sender.Repositories
+{
+    public class CommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Comment> GetCommentsByClimbId(int id)
+        {
+            return _context.Comment.Include(c => c.Climb)
+                            .Include(c => c.UserProfile)
+                            .Where(c => c.ClimbId == id)
+                            .OrderByDescending(c => c.DateCreated)
+                            .ToList();
+        }
+
+        public void AddComment(Comment comment)
+        {
+            _context.Add(comment);
+            _context.SaveChanges();
+        }
+
+        public void DeleteComment(int id)
+        {
+            var comment = _context.Comment.FirstOrDefault(c => c.Id == id);
+            _context.Comment.Remove(comment);
+            _context.SaveChanges();
+
+        }
+        public void Update(Comment comment)
+        {
+            _context.Entry(comment).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
