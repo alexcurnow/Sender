eb4f3f7f75db75f30ff8f0915832523287910fdf
Alex Curnow <alexcurnow@gmail.com>
2020-07-31 14:38:39 -0500

    user is now taken back to the dashboard once they find all the climb moves aka solve it

--- Sender/client/src/components/climb/Solution.js
+++ Sender/client/src/components/climb/Solution.js
@@ -9,6 +9,8 @@ export const Solution = () => {
   const parsedId = parseInt(id);
   const history = useHistory();
 
+  const [clickCount, setClickCount] = useState(1);
+
   const { currentClimb, getClimbById } = useContext(ClimbContext);
   const { moves, getMovesByClimbId } = useContext(MoveContext);
 
@@ -21,8 +23,6 @@ export const Solution = () => {
     getMovesByClimbId(parsedId);
 
     const canvas = canvasRef.current;
-    canvas.style.height = "1000px";
-    canvas.style.width = "800px";
 
     const ctx = canvas.getContext("2d");
     contextRef.current = ctx;
@@ -32,22 +32,54 @@ export const Solution = () => {
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
+      canvas.style.height = "1000px";
+      canvas.style.width = "800px";
       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     };
   }, []);
 
+  // const drawCircle = (e) => {
+  //   const canvas = canvasRef.current;
+
+  //   const ctx = canvas.getContext("2d");
+  //   contextRef.current = ctx;
+
+  //   const x = e.nativeEvent.offsetX;
+  //   const y = e.nativeEvent.offsetY;
+  //   const r = 50;
+
+  //   ctx.beginPath();
+  //   ctx.arc(x, y, r, Math.PI * 2, false);
+  //   ctx.strokeStyle = "blue";
+  //   ctx.stroke();
+  // };
+
+  const alertComplete = (moves) => {
+    const finalMove = moves.slice(-1)[0];
+    alert(
+      `Congrats, you found the final move for this climb: a ${finalMove.limb.name} hold! Now go send another one!`
+    );
+  };
+
   const answerChecker = (e) => {
-    moves.forEach((move) => {
-      if (
-        e.nativeEvent.offsetX >= move.xcoord - 20 &&
-        e.nativeEvent.offsetX <= move.xcoord + 20 &&
-        e.nativeEvent.offsetY >= move.ycoord - 20 &&
-        e.nativeEvent.offsetY <= move.ycoord + 20
-      )
-        alert(
-          `You found move ${move.sequenceNumber} of ${moves.length}! Looks like it's a ${move.limb.name} hold!`
-        );
-    });
+    if (clickCount !== moves.length) {
+      moves.forEach((move) => {
+        if (
+          e.nativeEvent.offsetX >= move.xcoord - 20 &&
+          e.nativeEvent.offsetX <= move.xcoord + 20 &&
+          e.nativeEvent.offsetY >= move.ycoord - 20 &&
+          e.nativeEvent.offsetY <= move.ycoord + 20
+        ) {
+          alert(
+            `You found move ${move.sequenceNumber} of ${moves.length}! Looks like it's a ${move.limb.name} hold!`
+          );
+          setClickCount(clickCount + 1);
+        }
+      });
+    } else if (clickCount === moves.length) {
+      alertComplete(moves);
+      history.push("/");
+    }
   };
 
   return (
@@ -66,6 +98,7 @@ export const Solution = () => {
         <canvas
           onClick={(e) => {
             answerChecker(e);
+            // drawCircle(e);
           }}
           ref={canvasRef}
         ></canvas>
